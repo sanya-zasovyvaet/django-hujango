64965016c896853a1235f4cb1c4a92cefa541a72
Alexander Modeev <modeev@lesdozor.ru>
2018-11-26 11:10:00 +0300

    Pleeease

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '../static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
